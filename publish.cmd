626bd1640f4e5cb5eae12ab3afcb7646fab2dc7e
Minh Nhat <nult2003@gmail.com>
2018-12-13 16:58:48 +0700

    test
    test

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=hello1nultbot-Web-Deploy.pubxml -p:Password=qlshRFsH5cCWnff82WYxBGvh01pZxt7hRwiw5hjYRW1EFSvFw7Rii0P8mPKu
+
